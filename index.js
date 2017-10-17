02261d72ad14f4f01b786866dec992688cc2dce8
mikanebu <meiran1991@gmail.com>
2017-10-17 10:58:52 +0600

    [graph-table-test][m]: added graph, table tests

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 const fs = require('fs')
 
 const json2csv = require('json2csv')
-const cheerio = require('cheerio')
 const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const puppeteer = require('puppeteer');
 
-var newLine= "\r\n"
+
+let newLine= "\r\n"
 const datasetsToTest = [
   {
     "owner": "core",
-    "name": "finance-vix"
+    "name": "gdp-uk"
   },
   {
     "owner": "core",
-    "name": "gdp"
+    "name": "gdp-us"
   }
 ]
 
 const baseUrl = 'https://datahub.io'
 
 const checkPage = async (url) => {
-  const info = {}
   const res = await fetch(url)
-  let body = await res.text()
-  info.status = res.status
-  info.htmlBody = body
-  return info
+  return res
 }
 
 const datapackageJson = async (url) => {
@@ -34,23 +32,41 @@ const datapackageJson = async (url) => {
   return body
 }
 
-const resourceLinks = async (url) => {
-  const res = await fetch(url)
-  return res
+const pageLoadTime = async (url) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  let metrics = await page.getMetrics()
+  await browser.close()
+  return metrics
 }
 
-const writeToCSV = async (fields, toCsv) => {
-  var csv = json2csv(toCsv) + newLine;
+const pageContent = async (url) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  let content = await page.content()
+  await browser.close()
+  return content
+}
 
-  await fs.appendFile('status.csv', csv, err => {
+const writeToCSV = (statuses) => {
+  let fields = ["id","name","page_status","page_title","dataset_title","readme","csv_links","csv_preview_links","json_links","zip_links","datapackage_json","total_load_time","script_laod_time","layout_load_time","tables","graphs"]
+  let toCsv = {
+      data: statuses,
+      fields: fields,
+      hasCSVColumnTitle: false
+  }  
+  let csv = json2csv(toCsv) + newLine;
+
+  fs.appendFile('status.csv', csv, err => {
       if (err) {
         console.log(err)
       }
       console.log('The data appended to file!');
-  });
+  })
 }
 
-//checkShowcase page returns 200 and get html content
 datasetsToTest.forEach(async dataset => {
   const statuses = {}
   const date = new Date()
@@ -59,12 +75,13 @@ datasetsToTest.forEach(async dataset => {
   const showcaseUrl = `https://datahub.io/${dataset.owner}/${dataset.name}`
   
   // page status 
-  const page = await checkPage(showcaseUrl) //returns {status: statusCode, body: htmlBody}
-  statuses.page_status = page.status
+  const page = await checkPage(showcaseUrl)
+  statuses.page_status = page.status + ':' + page.statusText
   if (page.status !== 200) {
-    // exit
+    
   } else {
-    const $ = cheerio.load(page.htmlBody)
+    const htmlBody = await pageContent(showcaseUrl)
+    const $ = await cheerio.load(htmlBody)
     let datackageUrl = `https://pkgstore.datahub.io/${dataset.owner}/${dataset.name}/latest/datapackage.json`
     const dp = await datapackageJson(datackageUrl)
     
@@ -95,21 +112,22 @@ datasetsToTest.forEach(async dataset => {
     // resources link
     const resourcesLink = $('.resource-listing').find('a')
     for (let i = 0; i < resourcesLink.length; i++) {
+      console.log(resourcesLink[i].attribs.href)
       if (resourcesLink[i].attribs.href.startsWith('/')) {
         switch(resourcesLink[i].attribs.href.substr(resourcesLink[i].attribs.href.lastIndexOf('.') + 1)) {
           case 'csv':
             let csvLinks = baseUrl + resourcesLink[i].attribs.href
-            const csvUrl = await resourceLinks(csvLinks)
+            const csvUrl = await checkPage(csvLinks)
             statuses.csv_links = csvUrl.status + ':' + csvUrl.statusText
             break;
           case 'json':
             let jsonLinks = baseUrl + resourcesLink[i].attribs.href
-            const jsonUrl = await resourceLinks(jsonLinks)
+            const jsonUrl = await checkPage(jsonLinks)
             statuses.json_links = jsonUrl.status + ':' + jsonUrl.statusText
             break;
           case 'zip':
             let zipLinks = baseUrl + resourcesLink[i].attribs.href
-            const zipUrl = await resourceLinks(zipLinks)
+            const zipUrl = await checkPage(zipLinks)
             statuses.zip_links = zipUrl.status + ':' + zipUrl.statusText
             break;
         }
@@ -123,24 +141,43 @@ datasetsToTest.forEach(async dataset => {
         const previewUrl = resource.path
         const resPreview = await fetch(previewUrl)
         statuses.csv_preview_links = resPreview.status + ': ' + resPreview.statusText
-      } 
+      } else if (!statuses.csv_preview_links){
+        statuses.csv_preview_links ='NOT GENERATED'
+      }
     }
     
     // datapackage_json
     const datapackageLink = $('.container').find('.btn-default')
     if ( datapackageLink.text() === 'Datapackage.json') {
       let datapackageUrl = baseUrl + datapackageLink[0].attribs.href
-      const dpUrl = await resourceLinks(datapackageUrl)
+      const dpUrl = await checkPage(datapackageUrl)
       statuses.datapackage_json = dpUrl.status + ':' + dpUrl.statusText
     }
-  }
+    // page loading time 
+    const loadTime = await pageLoadTime(showcaseUrl)
+    statuses.total_load_time = loadTime.TaskDuration
+    statuses.script_laod_time = loadTime.ScriptDuration
+    statuses.layout_load_time = loadTime.LayoutDuration
+    
+    // graphs
+    const graphC = $('.svg-container').css()
+    if (graphC.position === 'relative' && graphC.width === '670px' && graphC.height === '450px') {
+      statuses.graphs = 'OK'
+    } else {
+      statuses.graphs = 'NOT OK'
+    }
+    
+    // tables
+    const table = $('.htCore').find('tr').length
+    console.log(table)
+    if (table > 2) {
+      statuses.tables = 'OK'
+    } else {
+      statuses.tables = 'NOT OK'
+    }
+  }  
   
   // append row into csv file
-  let fields = ["id","name","page_status","page_title","dataset_title","readme","csv_links","csv_preview_links","json_links","zip_links","datapackage_json","total_load_time","first_byte_load_time","html_load_time","data_load_time","zip_content","tables","graphs"]
-  var toCsv = {
-      data: statuses,
-      fields: fields,
-      hasCSVColumnTitle: false
-  }
-  const appentToStatus = await writeToCSV(fields, toCsv)
+
+  const appentToStatus = writeToCSV(statuses)
 })
