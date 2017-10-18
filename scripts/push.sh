763cdafe3e61a44b49369b2d1262a41911719d3c
mikanebu <meiran1991@gmail.com>
2017-10-18 12:49:11 +0600

    [travis][xs]: updated with datopianbot token - refs #3

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "meiran1991@gmail.com"
-  git config --global user.name "mikanebu"
+  git config --global user.email "admin-login@datopian.com"
+  git config --global user.name "datopianbot"
 }
 
 commit_website_files() {
-  git checkout -b master
   git add status.csv
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git push origin master
+  git push origin master 
 }
 
 setup_git
